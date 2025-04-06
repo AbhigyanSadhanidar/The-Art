a4dee6d7c5459d0596bfd5b7fcbf8418e0467442
AbhigyanSadhanidar <abhigyansadhanidar@gmail.com>
2025-04-06 22:31:02 +0530

    Restructure: moved shopsy-main to root

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to The Art Demo</h1>
+      <p>This is your main App component running successfully!</p>
+    </div>
+  );
+};
+
+export default App;
